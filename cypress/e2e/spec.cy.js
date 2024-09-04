09133a5e3483b20a1c5fec3500c83b3f90bdd8c5
kyei-baffour <counsellor.kyei-baffour@amalitech.orggit config --global user.namekyei-baffourgit config --global user.email-baffour>
2024-09-04 16:04:17 +0000

    chamges to board

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,32 @@
+
+
+it('my first test',() => {
+  cy.visit('/board/1')
+//   /* ==== Generated with Cypress Studio ==== */
+//   cy.get('h2').click();
+//   cy.get('[data-cy="add-list-input"]').clear('n');
+//   cy.get('[data-cy="add-list-input"]').type('new list{enter}');
+//   /* ==== End Cypress Studio ==== */
+
+cy.get('[data-cy="add-list-input"]')
+.type('new list{enter}')
+cy.contains('Add another card')
+.click()
+cy.get('[data-cy="new-card-input"]')
+.type('new card {enter}')
+cy.contains('list')
+.click()
+})
+
+// it.only('fav', () => {
+//     cy.visit('/')
+//     cy.get('[data-cy="star"]')
+//     .first()
+//     .click({force: true})
+    
+// });
+it.only('test2', () => {
+  cy.visit('/')
+  
+
+});
